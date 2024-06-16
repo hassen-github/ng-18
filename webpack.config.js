b5536c9880b2a327c1e9afbf784f15e731aa884a
hassen-github <lyceett.hassen@gmail.com>
2024-06-16 19:03:33 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -26,3 +26,37 @@ module.exports = {
     }),
   ],
 };
+////////////////////////
+module.exports = {
+  output: {
+    publicPath: "https://hassen-github.github.io/ng-18-example/",
+    uniqueName: "fc"
+  },
+  optimization: {
+    // Only needed to bypass a temporary bug
+    runtimeChunk: false
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+
+      // For remotes (please adjust)
+      
+      name: "fc",
+      library: { type: "var", name: "fc" },
+      filename: "remoteEntry.js",
+      exposes: {
+        './web-components': './src/bootstrap.ts',
+      },
+      
+
+      shared: share({
+        "@angular/core": { requiredVersion:'auto' }, 
+        "@angular/common": { requiredVersion:'auto' }, 
+        "@angular/router": { requiredVersion:'auto' },
+        "rxjs": { requiredVersion:'auto' }
+      })
+    }
+
+    )
+  ]
+};
